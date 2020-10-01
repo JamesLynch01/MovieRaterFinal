422544bdfcdb79e919e314a13e54917e984859ba
JamesLynch01 <Jamesblynch94@gmail.com>
2020-10-01 13:42:59 -0500

    purging most of the unnecessary code

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -7,27 +7,18 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
-import { StudentsComponent } from './students/students.component';
 import { ApiAuthorizationModule } from 'src/api-authorization/api-authorization.module';
 import { AuthorizeGuard } from 'src/api-authorization/authorize.guard';
 import { AuthorizeInterceptor } from 'src/api-authorization/authorize.interceptor';
 import { TeacherComponent } from './teacher/teacher.component';
-import { MovieShowRoomComponent } from './movie-show-room/movie-show-room.component';
-import { MoviePiecesComponent } from './movie-pieces/movie-pieces.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
     HomeComponent,
-    CounterComponent,
-    FetchDataComponent,
-    StudentsComponent,
     TeacherComponent,
-    MovieShowRoomComponent,
-    MoviePiecesComponent
+    
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -36,11 +27,7 @@ import { MoviePiecesComponent } from './movie-pieces/movie-pieces.component';
     ApiAuthorizationModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-        { path: 'fetch-data', component: FetchDataComponent, canActivate: [AuthorizeGuard] },
-        { path: 'students', component: StudentsComponent, canActivate: [AuthorizeGuard] },
         { path: 'teachers', component: TeacherComponent, canActivate: [AuthorizeGuard] },
-        { path: 'movie-show-room', component: MovieShowRoomComponent, canActivate: [AuthorizeGuard]},
     ])
   ],
   providers: [
